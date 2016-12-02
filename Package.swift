ce835bedc3cbda01600b646b16c5a0d5421c7d15
Robert F. Dickerson <rfdickerson@gmail.com>
2016-12-02 13:14:31 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,5 +8,6 @@ let package = Package(
 		.Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", majorVersion: 0, minor: 2),
 		.Package(url: "https://github.com/davidungar/miniPromiseKit",        majorVersion: 4, minor: 2),
 		.Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git",        majorVersion: 1, minor: 8),
+	        .Package(url: "https://github.com/watson-developer-cloud/ios-sdk",   majorVersion: 0, minor: 10)
 	]
 )
